61373fea1082e4a3ea73ec138618cee551098052
Nick Romano <njromano86@gmail.com>
2016-09-26 01:04:51 -0400

    Fixed dumb GET requests to YouTube Added "Link" text to MainActivity to show clickable results (for now)

--- app/src/main/java/com/njromano/songyt/MainActivity.java
+++ app/src/main/java/com/njromano/songyt/MainActivity.java
@@ -9,19 +9,37 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    private String TAG = this.getClass().getSimpleName();
     private String ACTION_LISTENER = "com.njromano.songyt.NOTIFICATION_LISTENER";
     private String ACTION_SERVICE = "com.njromano.songyt.NOTIFICATION_SERVICE";
-    private TextView mSongText, mArtistText;
+    private TextView mSongText, mArtistText, mLinkText;
     private NotificationReceiver nReceiver;
     private Song mSong;
+    private StringRequest mYTRequest;
+    private ArrayList<YTResource> mYTResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +61,69 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mSong = new Song();
+        mYTResources = new ArrayList<>();
 
         mSongText = (TextView) findViewById(R.id.songtext);
         mArtistText = (TextView) findViewById(R.id.artisttext);
+        mLinkText = (TextView) findViewById(R.id.linktext);
 
         nReceiver = new NotificationReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_LISTENER);
         registerReceiver(nReceiver, filter);
+    }
+
+    private void getYoutubeInfo()
+    {
+        String url = "";
+        try {
+            url = "https://www.googleapis.com/youtube/v3/search"
+                    + "?part=snippet"
+                    + "&q=" + URLEncoder.encode(mSong.getArtist() + " " + mSong.getTitle(), "UTF-8")
+                    + "&type=video"
+                    + "&key=" + URLEncoder.encode(getResources().getString(R.string.API_KEY), "UTF-8");
+        } catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        mYTRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        Log.d(TAG, response);
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            mYTResources.clear();
+                            mYTResources.addAll(YTResource.fromJson(jsonResponse.getJSONArray("items")));
+                            if(mYTResources.isEmpty()) {
+                                Intent i = new Intent(ACTION_LISTENER);
+                                i.putExtra("error", "No matching songs on YouTube.");
+                                sendBroadcast(i);
+                            }
+                            else
+                            {
+                                mLinkText.setText("https://www.youtube.com/watch?v="
+                                        + mYTResources.get(0).getVideoId());
+                            }
+                        }catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        error.printStackTrace();
+                        Log.d(TAG, error.toString());
+                    }
+                });
+    }
+
+    public void makeSnackbar(String text)
+    {
+        Snackbar.make(mArtistText, text, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
@@ -113,6 +186,9 @@ public class MainActivity extends AppCompatActivity {
                 mSong.setArtist(intent.getStringExtra("artist_name"));
                 mSongText.setText(mSong.getTitle());
                 mArtistText.setText(mSong.getArtist());
+                getYoutubeInfo();
+                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(mYTRequest);
+                Log.d(TAG,mYTRequest.toString());
             }
         }
     }
---
+++ app/src/main/java/com/njromano/songyt/YTResource.java
@@ -0,0 +1,69 @@
+package com.njromano.songyt;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nick on 9/21/16.
+ * Class for YouTube Resources to be pulled from JSON
+ */
+public class YTResource {
+    private String videoId, description, title;
+
+    public YTResource(JSONObject object)
+    {
+        try {
+            this.videoId = object.getJSONObject("id").getString("videoId");
+            this.description = object.getJSONObject("snippet").getString("description");
+            this.title = object.getJSONObject("snippet").getString("title");
+        } catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<YTResource> fromJson(JSONArray jsonObjects)
+    {
+        ArrayList<YTResource> resources = new ArrayList<>();
+        for(int i=0; i< jsonObjects.length(); i++)
+        {
+            try {
+                resources.add(new YTResource(jsonObjects.getJSONObject(i)));
+            } catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return resources;
+    }
+
+    public String getVideoId()
+    {
+        return videoId;
+    }
+    public void setVideoId(String videoId)
+    {
+        this.videoId = videoId;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+}
